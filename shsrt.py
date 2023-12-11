a3a9ce4ade635fa344627b6b3905628d69e2a2a4
Abel Nagy <abel.nagy.m4tankor@gmail.com>
2023-12-11 18:48:03 +0100

    Initial commit.

---
+++ shsrt.py
@@ -0,0 +1,36 @@
+import re
+from datetime import datetime, timedelta
+
+def shift_time(time_str, shift_ms):
+    time_format = "%H:%M:%S,%f"
+    original_time = datetime.strptime(time_str, time_format)
+    shifted_time = original_time + timedelta(milliseconds=shift_ms)
+    return shifted_time.strftime(time_format)[:-3]  # Remove microseconds
+
+def shift_subtitles(input_file, output_file, shift_ms):
+    with open(input_file, 'r', encoding='utf-8') as infile:
+        lines = infile.readlines()
+
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for line in lines:
+            time_match = re.match(r'(\d+:\d+:\d+,\d+) --> (\d+:\d+:\d+,\d+)', line)
+            if time_match:
+                start_time, end_time = time_match.groups()
+                shifted_start_time = shift_time(start_time, shift_ms)
+                shifted_end_time = shift_time(end_time, shift_ms)
+                line = f"{shifted_start_time} --> {shifted_end_time}\n"
+
+            outfile.write(line)
+
+if __name__ == "__main__":
+    input_file = input("Enter the input SRT file path: ")
+    output_file = input("Enter the output SRT file path: ")
+    
+    try:
+        shift_ms = int(input("Enter the time shift in milliseconds (positive or negative): "))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for time shift.")
+        exit()
+
+    shift_subtitles(input_file, output_file, shift_ms)
+    print(f"Subtitle times shifted by {shift_ms} milliseconds. Output written to {output_file}")
